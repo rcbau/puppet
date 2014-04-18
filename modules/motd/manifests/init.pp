7362035f651b8e482853fdc7d753934dcad0e8b9
Joshua Hesketh <josh@nitrotech.org>
2014-04-18 18:50:45 +1000

    Add a message of the day template

---
+++ modules/motd/manifests/init.pp
@@ -0,0 +1,14 @@
+# == Class: rcbau::motd_tail
+#
+# Set the message of the day tail
+class rcbau::motd_tail (
+  $template = 'motd/motd.tail.erb',
+) {
+  file { '/etc/motd.tail':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template($template),
+  }
+}
